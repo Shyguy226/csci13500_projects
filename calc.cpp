e4c5204b9fcc18aec91a9f8565ae919342c46670
Andrew Giannico <40038319+Shyguy226@users.noreply.github.com>
2018-09-17 16:49:42 -0400

    Create calc.cpp

---
+++ calc.cpp
@@ -0,0 +1,26 @@
+/*
+Author: Andrew Giannico
+Course: CSCI-133
+Instructor: Mike Zamansky
+Assignment: Project 1 Task B
+This program reads a .txt file and reads plusses and minuses as well as numbers to properly assess the experession.
+*/
+#include <iostream>
+ using namespace std;
+ int main () {
+	char n;
+	int tot, temp;
+	int minus = 1;
+	while (cin >> temp){
+		if (cin.fail() ){
+			cin >> n;
+			if (n == '-')
+				minus = -1;
+			else
+				minus = 1;
+		}
+		tot += temp * minus;
+	}
+	cout << tot << endl;
+	return 0;
+}
