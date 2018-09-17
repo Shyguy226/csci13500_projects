1688437b137a8032080fd599be54d03b97f0f9f8
Andrew Giannico <40038319+Shyguy226@users.noreply.github.com>
2018-09-17 17:00:49 -0400

    Update calc.cpp

--- calc.cpp
+++ calc.cpp
@@ -6,22 +6,22 @@ Assignment: Project 1 Task B
 This program reads a .txt file and reads plusses and minuses as well as numbers to properly assess the experession.
 */
 #include <iostream>
- using namespace std;
- int main () {
-	char n;
+
+using namespace std;
+
+int main () {
+	char n; //need a char for '-' and '+'
 	int tot, temp;
-	int minus = 1;
-	int c = 0;
-	while (cin >> temp && c < 10){
+	int minus = 1; //keeps track of minus or plus
+	while (cin >> temp){
 		if (cin.fail() ){
 			cin >> n;
 			if (n == '-')
 				minus = -1;
 			else
-				minus = 1;
+				minus = 1; 
 		}
 		tot += temp * minus;
-		c++;
 	}
 	cout << tot << endl;
 	return 0;
