087f18378d32b544fcb1e5f45b7d9301ef52ec74
Andrew Giannico <40038319+Shyguy226@users.noreply.github.com>
2018-10-12 19:19:00 -0400

    Update pronounce.cpp

--- pronounce.cpp
+++ pronounce.cpp
@@ -2,8 +2,7 @@
 Author: Andrew Giannico
 Course: CSCI-135
 Instructor: Genady Maryash
-Assignment: Project 2 Phase II
-
+Assignment: Project 2 Phase III
 This program will take in a word and print out the pronunciation and words in the dictionary that have the exact same pronunciation.
 */
 #include <iostream>
@@ -28,6 +27,16 @@ void splitOnSpace(string s, string & before, string & after) {
   while (i < s.size()) { after += s[i]; i++; }
 }
 
+int countSpaces (string input){
+  int num = 0;
+  string space = " ";
+  for ( int i = 0; i < input.length(); i++){
+    if (input.substr(i,1) == space)
+      num++;
+  }
+  return num;
+}
+
 string Convert(string& str) //this will convert to capital letters
 {
 	locale settings;
@@ -41,34 +50,76 @@ string Convert(string& str) //this will convert to capital letters
 
 int main(){
 	string word = "";
-	string ex = "";
+	int counter = 0;
+	int spaceCount = 0;
+	string spaces = "";
 	bool found = false;
+	bool firsti = true;
 	string compare= "";
 	string insert = "";
 	string insert2 = "";
+	string insert3 = "";
+	string pro="";
+	string past="";
+	string r1 = "";
+	string r2 = "";
+	string r3 = "";
+	string r4 = "";
+	string r5 = "";
+	string r6 = "";
+	string a="";
+	string b="";
 	cin >> word;
-	string ans="Pronunciation	: ";
+	string ans="Pronunciation	:";
 	ifstream fin("Pro_Dictionary");
-	while (fin >> compare){
-		if (Convert(word) == compare){
-			fin >> ex;
-			getline(fin, insert);
-			found = true;
-			ans += ex + insert;
-			fin.close();
-		}
+	while (getline(fin,insert)){
+	  splitOnSpace(insert, a, b);
+	  if (a == Convert(word)){
+	    ans+=b;
+	    past = a;
+	    pro = b;
+	    found = true;
+	    fin.close();
+	  }
 	}
 	ans += "\n\nIdentical	: ";
-	while (fin >> compare){
-		getline(fin, insert2);
-		if (insert == insert2){
-			ans += compare;
+	fin.open ("Pro_Dictionary");
+	while (getline(fin, insert2)){
+	  splitOnSpace(insert2, a, b);
+		if (b == pro){
+		  if(Convert(past) != a)
+			ans += a + " ";
 		}
 	}
-	if (!found){
-		ans = "Not found";
+	fin.close();
+	spaceCount = countSpaces(pro);
+	ans += "\nReplace phoneme\t: ";
+	fin.open ("Pro_Dictionary");
+	while (getline(fin, insert3)){
+	  splitOnSpace(insert3, a, b);
+	  r2 = pro;
+	  r4 = b;
+	  if (spaceCount == countSpaces(r4)){
+	  for (int i = 0; i < spaceCount+1; i++){
+	    splitOnSpace(r2, r1, r2);
+	    splitOnSpace(r4, r3, r4);
+	    if (r1 != r3){
+	      counter++;
+	    }
+	    
+	  }
+	  if (counter < 2 && a != past){
+	    ans += a + " ";
+	  }
+	  counter = 0;
+	  }
 	}
+	    
+	if (!found){
+	ans = "Not found";
+	}
+	
 	cout << ans << endl;
-	return 0;
-		
+	return 0;	
+	
 }
